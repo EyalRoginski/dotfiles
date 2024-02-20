5bf8faa6f1a5b390a66ea3a29cb79093c716aa94
Eyal Roginski <eyalrog1@gmail.com>
2024-02-20 10:34:31 +0200

    Added <leader>fa keymap in vim to search all files including ignored and hidden.

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,12 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fa', function()
+    builtin.find_files({
+        hidden = true,
+        no_ignore = true,
+        no_ignore_parent = true
+    })
+end, {})
 vim.keymap.set('n', '<leader>fp', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fm', builtin.buffers, {})
