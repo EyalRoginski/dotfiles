c7e3bd4841ef90845e3acd6c93b9050021ffe8d8
Eyal Roginski <eyalrog1@gmail.com>
2024-02-21 12:44:26 +0200

    Added <leader>W to save without formatting

--- .config/nvim/lua/roginski/remap.lua
+++ .config/nvim/lua/roginski/remap.lua
@@ -1,6 +1,9 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>w", vim.cmd.up)
+vim.keymap.set("n", "<leader>W", function()
+    vim.cmd("noa up")
+end)
 
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
