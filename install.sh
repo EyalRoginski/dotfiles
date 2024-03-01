840d7ab537ab04b8fba02a86f70b6055d856e25d
Eyal Roginski <eyalrog1@gmail.com>
2024-03-01 23:36:55 +0200

    Added install script

---
+++ install.sh
@@ -0,0 +1,43 @@
+# You should set your terminal to a compatible font first - I use 0xProto Nerd Font
+
+# Install zsh
+if !(zsh --version); then
+    apt install zsh
+fi
+
+# Set zsh to be default shell
+if !(echo $SHELL | grep -q zsh); then
+    chsh -s $(which zsh)
+fi
+
+# Install oh-my-zsh
+if [ ! -f $HOME/.oh-my-zsh ]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# Install p10k
+if [ ! -d $HOME/.oh-my-zsh/custom/themes/powerlevel10k ]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
+
+# Install TPM
+if [ ! -d $HOME/.tmux/plugins/tpm ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+# Install latest Neovim
+if !(nvim --version); then
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+    chmod u+x nvim.appimage
+    ./nvim.appimage
+fi
+
+
+# Copy dotfiles
+cp ./.tmux.conf $HOME/.tmux.conf
+cp ./.zshrc $HOME/.zshrc
+cp ./.aliases $HOME/.aliases
+cp ./.p10k.zsh $HOME/.p10k.zsh
+
+mkdir -p $HOME/.config/ 2>/dev/null
+cp -R ./.config/nvim $HOME/.config/
