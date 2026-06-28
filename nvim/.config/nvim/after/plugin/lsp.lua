9b7cd21996dd3eb5240e77d07b2e1e451315fa40
Eyal Roginski <eyalrog1@gmail.com>
2026-06-28 15:58:57 +0300

    Make basedpyright available on portable config

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -3,8 +3,8 @@ local is_portable = vim.env.NVIM_PORTABLE == "1"
 vim.lsp.enable("rust_analyzer")
 vim.lsp.enable("lua_ls")
 vim.lsp.enable("ruff")
+vim.lsp.enable("basedpyright")
 if not is_portable then
-    vim.lsp.enable("basedpyright")
     vim.lsp.enable("omnisharp")
     vim.lsp.enable("bashls")
 end
